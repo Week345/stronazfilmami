b4cfdb97003c3f88ef5f03afaae34445808f4383
Wiktor Kulikowski <wiktorere@outlook.com>
2025-03-19 11:10:53 +0100

    added missing files

---
+++ src/main/java/com/filmpage/model/Category.java
@@ -0,0 +1,20 @@
+package com.filmpage.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Film> films;
+}
